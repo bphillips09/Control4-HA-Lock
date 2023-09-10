44f589da6126de86805db7daef6007e56e93c001
Brandon Phillips <bphillips09@me.com>
2023-09-10 01:01:09 -0500

    Initial commit

---
+++ commands.lua
@@ -0,0 +1,75 @@
+function RFP.RECEIEVE_STATE(idBinding, strCommand, tParams)
+    local jsonData = JSON:decode(tParams.response)
+
+    local stateData
+
+    if jsonData ~= nil then
+        stateData = jsonData
+    end
+
+    Parse(stateData)
+end
+
+function RFP.RECEIEVE_EVENT(idBinding, strCommand, tParams)
+    local jsonData = JSON:decode(tParams.data)
+
+    local eventData
+
+    if jsonData ~= nil then
+        eventData = jsonData["event"]["data"]["new_state"]
+    end
+
+    Parse(eventData)
+end
+
+function Parse(data)
+    if data == nil then
+        print("NO DATA")
+        return
+    end
+
+    if data["entity_id"] ~= EntityID then
+        return
+    end
+
+    local attributes = data["attributes"]
+    local state = data["state"]
+
+    if not Connected then
+        C4:SendToProxy(5001, 'LOCK_STATUS_CHANGED', { LOCK_STATUS = LOCK_STATE }, "NOTIFY")
+        Connected = true
+    end
+
+    if attributes == nil then
+        C4:SendToProxy(5001, 'LOCK_STATUS_CHANGED', { LOCK_STATUS = "unknown" }, "NOTIFY")
+        return
+    end
+
+    if attributes["available"] ~= nil then
+        local message = attributes["available"]
+        if message == "false" then
+            C4:SendToProxy(5001, 'LOCK_STATUS_CHANGED', { LOCK_STATUS = "unknown" }, "NOTIFY")
+        end
+    end
+
+    if attributes["lock_battery"] ~= nil then
+        local message = attributes["lock_battery"]
+
+        local batteryLevel = tonumber(message)
+        local batteryString = ""
+        if batteryLevel > 50 then
+            batteryString = "normal"
+        elseif batteryLevel > 25 then
+            batteryString = "warning"
+        else
+            batteryString = "critical"
+        end
+
+        C4:SendToProxy(5001, 'BATTERY_STATUS_CHANGED', { BATTERY_STATUS = batteryString }, "NOTIFY")
+    end
+
+    if state ~= nil then
+        C4:SendToProxy(5001, 'LOCK_STATUS_CHANGED', { LOCK_STATUS = state }, "NOTIFY")
+        LOCK_STATE = state
+    end
+end
